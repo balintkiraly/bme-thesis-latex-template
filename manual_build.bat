5c54b2857a82e1c74fbc9bb7d5cf6f72edaeeb4e
balintkiraly <balint.kiraly@protonmail.com>
2020-10-07 20:28:04 +0200

    Remove unnecessary files

--- manual_build.bat
+++
@@ -1,8 +0,0 @@
-pdflatex --quiet %DOCUMENT%
-bibtex -quiet %DOCUMENT%
-pdflatex --quiet %DOCUMENT%
-pdflatex --quiet %DOCUMENT%
-
-@REM move %DOCUMENT%.pdf ../pdf/%DOCUMENT%.pdf
-
-@del  *.aux *.dvi *.thm *.lof *.log *.lot *.fls *.out *.toc *.bbl *.blg
